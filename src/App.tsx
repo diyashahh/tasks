f74fe4996fafe3c72a681b73b805d94fb7a0583d
diyashah <diyashah@udel.edu>
2023-03-09 18:38:51 -0500

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -17,49 +17,6 @@ function App(): JSX.Element {
                 Diya Shah - UD CISC275 with React Hooks and TypeScript - Hello
                 World!
             </header>
-            {/* <<<<<<< HEAD
-            <Container>
-                <Row>
-                    <Col>
-                        {" "}
-                        To do list:
-                        <ul>
-                            <li>Clean Room </li>
-                            <li>Do Homework</li>
-                            <li>Go to grocery store</li>
-                            <li>Fill gas</li>
-                        </ul>
-                        <div
-                            style={{
-                                width: "175px",
-                                height: "50px",
-                                backgroundColor: "red"
-                            }}
-                        ></div>
-                    </Col>
-                    <Col>
-                        <img
-                            src="https://free-images.com/tn/366a/beachy_head_lighthouse_east_2.jpg"
-                            alt="A picture of a lighthouse"
-                        />
-                        <div
-                            style={{
-                                width: "175px",
-                                height: "50px",
-                                backgroundColor: "red"
-                            }}
-                        ></div>
-                    </Col>
-                </Row>
-            </Container>
-            <Button onClick={() => console.log("Hello World!")}>
-                Log Hello World
-            </Button>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Diya Shah.
-            </p>
-======= */}
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -72,7 +29,6 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
-            upstream/task-state
         </div>
     );
 }
--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,24 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [question, setQuestion] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    function type(): void {
+        setQuestion(
+            // If it's SA, make MC
+            question === "short_answer_question"
+                ? "multiple_choice_question"
+                : "short_answer_question"
+        );
+    }
+
+    return (
+        <div>
+            <Button onClick={type}>Change Type</Button>
+            {question === "multiple_choice_question"
+                ? "Multiple Choice"
+                : "Short Answer"}
+        </div>
+    );
 }
